3312beef8494ff67a2f44bab4f7147be63ffdcfd
Waldoox <kevin.barria1@utp.ac.pa>
2023-12-14 17:48:57 -0500

    Nav, head y footer responsive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-const preguntas = document.querySelectorAll(".pregunta_encabezado");
 
-preguntas.forEach((pregunta) => {
-	pregunta.addEventListener("click", () => {
-		removerClaseActivo();
-		pregunta.nextElementSibling.classList.add("activo");
-	});
-});
-
-function removerClaseActivo() {
-	preguntas.forEach((pregunta) => {
-		pregunta.nextElementSibling.classList.remove("activo");
-	});
+function hamburguer_nav(){
+	const sidebar = document.getElementById('sidebar')
+	if(sidebar.style.display === 'block'){
+		sidebar.style.display = 'none'
+	}
+	else{
+		sidebar.style.display = 'block'
+	}
 }
+
+// Función para manejar el cambio en el tamaño de la pantalla
+function handleScreenSizeChange(mq) {
+    const sidebar = document.querySelector('.sidebar');
+
+    // Verifica si la pantalla es lo suficientemente grande y el sidebar está cerrado
+    if (mq.matches && getComputedStyle(sidebar).display === 'none') {
+        sidebar.style.display = 'block'; // Abre el sidebar
+    } else {
+        sidebar.style.display = ''; // Restablece el estilo, permitiendo que el CSS tome el control
+    }
+}
+
+// Crea un media query para pantallas pequeñas
+const mq = window.matchMedia('(max-width: 768px)');
+
+// Llama a la función al cargar la página
+handleScreenSizeChange(mq);
+
+// Agrega un listener para manejar cambios en el tamaño de la pantalla
+mq.addListener(handleScreenSizeChange);
