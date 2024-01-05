2557b9cfe1568a4d3e8165a41f6720e0183974e7
Waldoox <kevin.barria1@utp.ac.pa>
2024-01-05 12:56:31 -0500

    Inicio de sesion

---
+++ demo/src/main/java/com/sakurapanama/demo/auth/AuthService.java
@@ -0,0 +1,19 @@
+package com.sakurapanama.demo.auth;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        return null;
+    }
+
+}
---
+++ demo/src/main/java/com/sakurapanama/demo/models/Role.java
@@ -0,0 +1,6 @@
+package com.sakurapanama.demo.models;
+
+public enum Role {
+    ADMIN,
+    USER
+}
