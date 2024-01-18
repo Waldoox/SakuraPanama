ddf998e0ae8ce787399a2ec28e7ed62087402f65
SueAcosta <sue.acosta@utp.ac.pa>
2024-01-18 16:31:02 -0500

    ayudaaaa

---
+++ demo/src/main/java/com/sakurapanama/demo/models/Lugar.java
@@ -0,0 +1,21 @@
+
+package com.sakurapanama.demo.models;
+
+public class Lugar {
+    private String lugar_img;
+
+    public Lugar(String lugar_img) {
+        this.lugar_img = lugar_img;
+    }
+
+    public Lugar() {
+    }
+
+    public String getLugar_img() {
+        return lugar_img;
+    }
+
+    public void setLugar_img(String lugar_img) {
+        this.lugar_img = lugar_img;
+    }
+} 
