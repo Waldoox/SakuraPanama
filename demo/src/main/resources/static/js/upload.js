684bcf9f2f3f988086890acc76fb3e895f3ea771
SueAcosta <sue.acosta@utp.ac.pa>
2024-01-29 14:33:50 -0500

    ayudaaa

--- demo/src/main/resources/static/js/upload.js
+++ demo/src/main/resources/static/js/upload.js
@@ -1,6 +1,9 @@
 //LUGARES
 let baseUrl = "http://localhost:8080";
 let lugares = [];
+let articulos = [];
+let eventos = [];
+
 
 function obtenerLugares() {
   // Modifica la URL para incluir el parámetro de filtrado por tipolocal
@@ -11,8 +14,6 @@ function obtenerLugares() {
     });
   });
 }
-
-
 function imprimirLugares() {
   let contenedor = document.getElementById("lugar");
   contenedor.innerHTML = "";
@@ -24,14 +25,72 @@ function imprimirLugares() {
   });
 
 }
-
 function mapearLugar(lugar) {
   return `<div class="secciones" id="lugar">
+    <img src="${lugar.lugar_img.id_tipolocal=1}" >
+  </div>`;
+}
+
+
+//ARTICULOS
+function obtenerLugares() {
+  // Modifica la URL para incluir el parámetro de filtrado por tipolocal
+  fetch(baseUrl + '/lugares?tipolocal=2').then(res => {
+    res.json().then(json => {
+      articulos = json;
+      imprimirLugares();
+    });
+  });
+}
+function imprimirLugares() {
+  let contenedor = document.getElementById("lugar");
+  contenedor.innerHTML = "";
+
+  let lugaresAleatorios = shuffleArray(articulos).slice(0, 7);
+
+  lugaresAleatorios.forEach(lugar => {
+    contenedor.innerHTML += mapearLugar(lugar);
+  });
+
+}
+function mapearLugar(lugar) {
+  return `<div class="secciones" id="ariculo">
     <img src="${lugar.lugar_img}" >
   </div>`;
 }
 
-//CARROUSEL
+
+//EVENTOS
+function obtenerLugares() {
+  // Modifica la URL para incluir el parámetro de filtrado por tipolocal
+  fetch(baseUrl + '/lugares?tipolocal=3').then(res => {
+    res.json().then(json => {
+      eventos = json;
+      imprimirLugares();
+    });
+  });
+}
+function imprimirLugares() {
+  let contenedor = document.getElementById("lugar");
+  contenedor.innerHTML = "";
+
+  let lugaresAleatorios = shuffleArray(eventos).slice(0, 7);
+
+  lugaresAleatorios.forEach(lugar => {
+    contenedor.innerHTML += mapearLugar(lugar);
+  });
+
+}
+function mapearLugar(lugar) {
+  return `<div class="secciones" id="evento">
+    <img src="${lugar.lugar_img}" >
+  </div>`;
+}
+
+
+
+
+//-----------------------CARROUSEL----------------------------------------------------
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -71,36 +130,3 @@ function prevSlide() {
   showSlide(currentIndex - 1);
 }
 
-
-
-//MERCADO
-let articulos = [];
-
-function obtenerArticulos() {
-    fetch(baseUrl + '/imagenesArticulos').then(res => {
-        res.json().then(json => {
-            articulos = json;
-            imprimirArticulos();
-        });
-    });
-}
-
-function imprimirArticulos() {
-    let contenedor = document.getElementById("articulos");
-    contenedor.innerHTML = "";
-
-    articulos.forEach(articulo => {
-        contenedor.innerHTML += mapearArticulo(articulo);
-    });
-}
-
-function mapearArticulo(imagen) {
-    return `<div class="secciones" id="mercado">
-        <img src="${imagen}" alt="Imagen del artículo">
-    </div>`;
-}
-
-// Función para cargar las imágenes al cargar la página
-window.onload = function() {
-    obtenerArticulos();
-};
